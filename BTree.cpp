d2d29e105289d34a6537f2b58dbafcb597e41740
Claustrophobia <yanxi@umail.ucsb.edu>
2016-02-27 18:26:21 -0800

    cannot compile yet but with basic logic

--- BTree.cpp
+++ BTree.cpp
@@ -2,11 +2,74 @@
 
 TreeNode::TreeNode(){
   isLeaf = false;
-  isRoot = false;
-  name = "";
-  index = -1;
 
-  leaves = new TreeNode* [L];
+  index = -1;
+  nameIndex = new string[M-1];
+  for(int i = 0; i < M-1; i++){
+    nameIndex[i] = "";
+  }
+
+  leaves = new pair<string, int>* [L];
   for(int i = 0; i < L; i++)
     leaves[i] = NULL;
+
+
+}
+
+TreeNode::TreeNode(string name, int index){
+  isLeaf = true;
+
+  nameIndex = new string[M-1];
+  for(int i = 0; i < M-1; i++){
+    nameIndex[i] = "";
+  }
+
+  leaves = new pair<string, int>* [L];
+  for(int i = 0; i < L; i++)
+    leaves[i] = NULL;
+
+  nextNodes = new TreeNode* [M];
+  for(int i = 0; i < M; i++)
+    nextNodes[i] = NULL;
+
+  pair<string, int>* temp = new pair<string, int>(name, index);
+  leaves[0] = temp;
+}
+
+BTree::BTree(){
+  root = NULL;
+}
+
+pair<pair<string, int>, bool>* BTree::find(string name){
+  TreeNode* current = root;
+  int index = 0;
+  while(current && !current->checkLeaf() && current->getNameIndex()){
+    while(index < M-1 && current->getNameIndex()[index].length() != 0 && name >= current->getNameIndex()[index])
+      index ++;
+    current = current->getNextNodes()[index];
+  }
+  index = 0;
+  while(index < L && current->getLeaves()[index] != NULL
+        && name > current->getLeaves()[index]->first){
+    index ++;
+  }
+
+  pair<string, int> temp(leaves[index]);
+
+  if(name == current->getLeaves()[index]->first){
+    pair<pair<string, int>, bool>* result = new pair<pair<string, int>, bool>(temp, true);
+    return result;
+  }
+  pair<pair<string, int>, bool>* result = new pair<pair<string, int>, bool>
+    (temp, false);
+  return result;
+}
+
+void BTree::insert(string name, int index){
+  if (root == NULL){
+    TreeNode* temp = new TreeNode(name, index);
+    root = temp;
+    return;
+  }
+
 }
--- BTree.h
+++ BTree.h
@@ -6,16 +6,32 @@ using namespace std;
 
 class TreeNode{
  private:
-  static const int M = 5;
-  static const int L = 3;
-  TreeNode** names;
-  TreeNode** indexArray;
-  TreeNode** leaves;
+  const int M = 5;
+  const int L = 3;
+  string* nameIndex;
+  TreeNode** nextNodes;
+  pair<string, int>** leaves;
   bool isLeaf;
-  bool isRoot;
-  string name;
   int index;
  public:
   TreeNode();
 
-}
+  //constructor for a leaf
+  TreeNode(string name, int index);
+
+  bool checkLeaf(){return isLeaf;}
+  void setLeaf(bool value){isLeaf = value;}
+
+  string* getNameIndex(){return nameIndex;}
+  TreeNode** getNextNodes(){return nextNodes;}
+  pair<string, int>** getLeaves(){return leaves;}
+};
+
+class BTree{
+ private:
+  TreeNode* root;
+ public:
+  BTree();
+  void insert(string name, int index);
+  pair<pair<string, int>, bool>* find(string name);
+};
